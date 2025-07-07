de9ef01781cc8672957a0bebf34e1e4b74bd7b18
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-07-07 21:52:27 +0545

    use new bucket

--- upload.py
+++ upload.py
@@ -5,15 +5,14 @@ import sys
 from glob import glob
 from subprocess import STDOUT, CalledProcessError, check_output
 
-DEST = "s3://dvc-public/dvc-pkgs/exe/"
-
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="path to the executable to upload")
+parser.add_argument("dest", help="destination S3 path")
 args = parser.parse_args()
 
 (path,) = glob(args.path)
 
-dest = posixpath.join(DEST, os.path.basename(path))
+dest = posixpath.join(args.dest, os.path.basename(path))
 
 try:
     out = check_output(f"aws s3 cp {path} {dest}", stderr=STDOUT, shell=True)
